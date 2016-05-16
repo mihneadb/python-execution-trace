8d81d605104d360fbfd3341574e4ecf7f1795cd9
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2016-05-15 22:24:21 -0700

    Minimal display layout for values.

--- viewer/static/js/viewer.js
+++ viewer/static/js/viewer.js
@@ -1,6 +1,7 @@
 var codemirror = null;
 var state = null;
 var stateIdx = -1;
+var varStateTemplate = Handlebars.compile($("#var-state-template").html());
 
 
 function initSourceViewer() {
@@ -21,9 +22,20 @@ function renderState() {
     var currentState = state[stateIdx].state;
     // Off by 1 because of how Python counts lines.
     var lineno = state[stateIdx].lineno - 1;
-    var rendered = JSON.stringify(currentState)
 
-    $('#state').text(rendered);
+    $('.state-viewer').empty();
+    var vars = Object.keys(currentState);
+    for (i = 0; i < vars.length; i++) {
+        var value = formatStateValue(currentState[vars[i]])
+        console.log(vars[i]);
+        var html = varStateTemplate({
+                               name: vars[i],
+                               value: value
+                           });
+        $('.state-viewer').append(html);
+        console.log(html);
+    }
+
     highlightLine(lineno);
 }
 
@@ -62,6 +74,14 @@ function keyHandler(event) {
         nextStep();
         return
     }
+}
+
+function formatStateValue(value) {
+    // remove leading and ending `"`, if any.
+    if (value[0] == '"' && value[value.length - 1]) {
+        return value.slice(1, value.length - 1);
+    }
+    return value;
 }
 
 
