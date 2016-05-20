31152cf45d2f622a9533ae72e9b0d09d634df3fb
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2016-05-19 21:24:22 -0700

    Use native slider.

--- example.py
+++ example.py
@@ -4,6 +4,47 @@ from trace.record import record
 @record
 def foo(x, y):
     a = x + y
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
+    #simulating a fn with many lines and around 80 chars per line here we go go
     for i in range(a):
         x = x + 1
     return x
